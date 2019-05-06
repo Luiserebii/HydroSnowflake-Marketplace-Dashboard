f778b1265ea75d69c5247adadb4b13d37ff35745
Luiserebii <luis@serebii.io>
2019-05-06 01:07:24 -0400

    Ok, slightly more advanced example built! :D

--- src/components/resolvers/Rinkeby/0x5181d8ad751731e7b90AD5d8cFdBa93CDDF3c8BE/CouponMarketplace.js
+++ src/components/resolvers/Rinkeby/0x5181d8ad751731e7b90AD5d8cFdBa93CDDF3c8BE/CouponMarketplace.js
@@ -1,19 +1,32 @@
-//
-import React/*, { useState }*/ from 'react';
+
+import React, { useState } from 'react';
 
 
 export default function testComponent({ ein }) {
-  
-  return (
-    <a href="serebii.io">{"Your EIN, uguu: " + ein}</a>
 
+  let [ meme, setMeme ] = useState(calculateNewMeme(ein));
+  
+  //Generate a new Meme ID number on each reload
+  //setMeme(calculateNewMeme(ein));
+
+  
+
+  return (
+    <div>
+      <a href="serebii.io">{"Your EIN, uguu: " + ein}</a>
+      <span>{"And your glorious MemeID number: " + meme}</span>
+    </div>
   );
   
   
   
-  
-  
+ 
   
   
 }
 
+
+function calculateNewMeme(num) {
+  return num * 2 * Math.floor(Math.random() * 10);
+};
+ 
