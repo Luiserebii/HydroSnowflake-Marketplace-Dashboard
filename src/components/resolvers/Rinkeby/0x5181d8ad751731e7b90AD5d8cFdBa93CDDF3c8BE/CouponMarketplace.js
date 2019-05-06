fea4610efcd0da2f181a23819e4139ba1c413eae
Luiserebii <luis@serebii.io>
2019-05-06 02:15:03 -0400

    Experimentation with a random number generator in React with Material UI

--- src/components/resolvers/Rinkeby/0x5181d8ad751731e7b90AD5d8cFdBa93CDDF3c8BE/CouponMarketplace.js
+++ src/components/resolvers/Rinkeby/0x5181d8ad751731e7b90AD5d8cFdBa93CDDF3c8BE/CouponMarketplace.js
@@ -1,11 +1,13 @@
 
 import React, { useState } from 'react';
-
+import { Typography, Input, Button } from '@material-ui/core';
 
 export default function testComponent({ ein }) {
 
   let [ meme, setMeme ] = useState(calculateNewMeme(ein));
-  
+  let [ upperBound, setUpperBound ] = useState(1);  
+  let [ result, setResult ] = useState('');
+
   //Generate a new Meme ID number on each reload
   //setMeme(calculateNewMeme(ein));
 
@@ -13,8 +15,26 @@ export default function testComponent({ ein }) {
 
   return (
     <div>
-      <a href="serebii.io">{"Your EIN, uguu: " + ein}</a>
-      <span>{"And your glorious MemeID number: " + meme}</span>
+      <Typography component="h1">
+        "Random Number Generator"
+      </Typography>
+      <Input 
+        id="numberMax"
+        placeholder="For example, type \"5\" to generate a number from 1-5"
+        required=true
+        onChange={e => setUpperBound(e.target.value)}
+      />
+      <Button
+        onClick={setResult(calcRandom(upperBound))}
+      >
+      <Typography id="result" component="h3">
+        {result != ''? "Your random number is: " + result : '') }
+      </Typography>
+      /*<a href="serebii.io">{"Your EIN, uguu: " + ein}</a>
+      <span>{"And your glorious MemeID number: " + meme}</span>*/
+      
+      
+      
     </div>
   );
   
@@ -27,6 +47,10 @@ export default function testComponent({ ein }) {
 
 
 function calculateNewMeme(num) {
-  return num * 2 * Math.floor(Math.random() * 10);
-};
+  return num * 2 * calcRandom(10);
+}
+
+function calcRandom(x) {
+  return Math.floor(Math.random() * x);
+}
  
