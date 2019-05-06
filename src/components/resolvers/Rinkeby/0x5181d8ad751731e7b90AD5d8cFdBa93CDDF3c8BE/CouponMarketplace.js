42cae1a090a708e8819da41eb97324cde68ffb23
Luiserebii <luis@serebii.io>
2019-05-06 00:51:59 -0400

    Finally, a working example!

---
+++ src/components/resolvers/Rinkeby/0x5181d8ad751731e7b90AD5d8cFdBa93CDDF3c8BE/CouponMarketplace.js
@@ -0,0 +1,19 @@
+//
+import React/*, { useState }*/ from 'react';
+
+
+export default function testComponent({ ein }) {
+  
+  return (
+    <a href="serebii.io">{"Your EIN, uguu: " + ein}</a>
+
+  );
+  
+  
+  
+  
+  
+  
+  
+}
+
