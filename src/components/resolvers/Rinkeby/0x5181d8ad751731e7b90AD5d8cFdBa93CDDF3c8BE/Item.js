42f5acfb3b01523a9d7e3cf878211abc660836dd
Luiserebii <luis@serebii.io>
2019-05-06 21:27:09 -0400

    Some corrections made in debugging

--- src/components/resolvers/Rinkeby/0x5181d8ad751731e7b90AD5d8cFdBa93CDDF3c8BE/Item.js
+++ src/components/resolvers/Rinkeby/0x5181d8ad751731e7b90AD5d8cFdBa93CDDF3c8BE/Item.js
@@ -2,34 +2,30 @@
 import React, { useState } from 'react';
 import { Typography } from '@material-ui/core';
 
-export default function Item({ ein }) {
+export default function Item(props) {
 
   return (
     <div>
       <Typography component="h1">
-        {this.props.title}
+        {props.title}
       </Typography>
       <ul>
-        <li>{this.props.description}</li>
-        <li>{this.props.price}</li>
-        <li>{this.props.delivery}</li>
-        { if(this.props.returnPolicy){ <li>this.props.returnPolicy</li> } }
+        <li>{props.description}</li>
+        <li>{props.price}</li>
+        <li>{props.delivery}</li>
+        <li>{props.returnPolicy}</li>
+
       </ul>
 
       <ul>
-        <li>UUID: {this.props.uuid}</li>
-        <li>QTY: {this.props.quantity}</li>
-        <li>Item Type: {this.props.itemType}</li>
-        <li>Item Status: {this.props.status}</li>
-        <li>Item Condition: {this.props.condition}</li>
+        <li>UUID: {props.uuid}</li>
+        <li>QTY: {props.quantity}</li>
+        <li>Item Type: {props.itemType}</li>
+        <li>Item Status: {props.status}</li>
+        <li>Item Condition: {props.condition}</li>
       </ul>
     </div>
   );
-
-);
-
-
-
 
 
 
--- src/components/resolvers/Rinkeby/0x5181d8ad751731e7b90AD5d8cFdBa93CDDF3c8BE/ItemList.js
+++ src/components/resolvers/Rinkeby/0x5181d8ad751731e7b90AD5d8cFdBa93CDDF3c8BE/ItemList.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 import Item from './Item';
 import { Typography, Input, Button } from '@material-ui/core';
 
-export default function ItemList({ ein }) {
+export default function ItemList(props) {
 
   return (
-    { generateItems(this.props.items) }
+    <div>
+      { generateItems(props.items) }
+    </div>
   );
-
-);
 
 }
 
@@ -17,25 +17,21 @@ function generateItems(items) {
 
     return (
       <Item
-        uuid = item.uuid
-        quantity = item.quantity
-        itemType = item.itemType
-        status = item.status
-        condition = item.condition
-        title = item.title
-        description = item.description
-        price = item.price
-        delivery = item.delivery
-        tags = item.tags
-        returnPolicy = item.returnPolicy
+        uuid = {item.uuid}
+        quantity = {item.quantity}
+        itemType = {item.itemType}
+        status = {item.status}
+        condition = {item.condition}
+        title = {item.title}
+        description = {item.description}
+        price = {item.price}
+        delivery = {item.delivery}
+        tags = {item.tags}
+        returnPolicy = {item.returnPolicy}
       />
 
     );
   });
-
-
-  );
-  
 
   return itemsRes;
 }
